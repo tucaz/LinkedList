dce39c86a9dfecba09343b6e8cece5deea9db516
tucaz <me@tucaz.net>
2011-07-05 15:39:52 -0300

    Added insertBefore and remove methods

--- LinkedList.js
+++ LinkedList.js
@@ -88,6 +88,71 @@ LinkedList.prototype.insertAfter = function(findValue, value)
 	}
 }
 
+LinkedList.prototype.insertBefore = function(findValue, value)
+{
+	var newNode = new Node(value);
+	var node = new Node(findValue);
+	
+	for(var currentNode = this.head; currentNode; currentNode = currentNode.getNext())
+	{
+		if(currentNode.getValue() === node.getValue())
+		{			
+			if(this.head != null && this.head === currentNode)
+			{
+				this.head.setPrevious(newNode);
+				newNode.setNext(this.head);
+				this.head = newNode;
+			}
+			else
+			{
+				var previousNode = currentNode.getPrevious();
+				previousNode.setNext(newNode)				
+				
+				currentNode.setPrevious(newNode);
+				
+				newNode.setNext(currentNode);
+				newNode.setPrevious(previousNode);
+			}
+			
+			break;
+		}
+	}
+}
+
+LinkedList.prototype.remove = function(findValue)
+{
+	var node = new Node(findValue);
+	
+	for(var currentNode = this.head; currentNode; currentNode = currentNode.getNext())
+	{
+		if(currentNode.getValue() === node.getValue())
+		{			
+			if(this.head != null && this.head === currentNode)
+			{
+				var nextNode = currentNode.getNext();
+				nextNode.setPrevious(null);
+				this.head = nextNode;
+			}
+			else if(this.tail != null && this.tail === currentNode)
+			{
+				var previousNode = currentNode.getPrevious();
+				previousNode.setNext(null);
+				this.tail = previousNode;
+			}
+			else
+			{
+				var nextNode = currentNode.getNext();
+				var previousNode = currentNode.getPrevious();
+				
+				nextNode.setPrevious(previousNode);
+				previousNode.setNext(nextNode);
+			}
+			
+			break;
+		}
+	}
+}
+
 LinkedList.prototype.replace = function(findValue, value)
 {	
 	var node = new Node(findValue);
@@ -106,12 +171,16 @@ LinkedList.prototype.replace = function(findValue, value)
 // ll.add(1);
 // ll.add(2);
 // ll.add(3);
+// ll.add(4);
+// ll.add(5);
 
 // for(var n = ll.head; n; n = n.getNext())
 	// console.log('Value: '+ n.getValue() + ' | Previous: ' + (n.getPrevious() == null ? '' : n.getPrevious().getValue()));
 
-// ll.insertAfter(new Node(3), 4);
-// ll.replace(new Node(3), 4);
+// // ll.remove(3);	
+// // ll.insertBefore(3, 2.5);
+// // ll.insertAfter(3, 4);
+// // ll.replace(3, 4);
 
 // for(var n = ll.head; n; n = n.getNext())
 	// console.log('Value: '+ n.getValue() + ' | Previous: ' + (n.getPrevious() == null ? '' : n.getPrevious().getValue()));
