afb4069d13b8a70102d4ba0f59b7a1aabdd4602e
tucaz <me@tucaz.net>
2011-07-11 18:04:39 -0300

    Added unit tests for the linked list Rewrote Node object to hide internals according to @rodbv suggestions

--- LinkedList.js
+++ LinkedList.js
@@ -1,189 +1,190 @@
-var Node = function (v) {
-	if (!(this instanceof arguments.callee)) {
-		throw new Error("Constructor called as a function");
-	}
-  
-	this.value = v;
-	this.next = null;
-	this.previous = null;
-}
-
-Node.prototype.setValue = function(v) {
-	this.value = v;
-}
-
-Node.prototype.getValue = function() {
-	return this.value;
-}
-
-Node.prototype.setNext = function(n) {
-	this.next = n;
-}
-
-Node.prototype.getNext = function() {
-	return this.next;
-}
-
-Node.prototype.setPrevious = function(p) {
-	this.previous = p;
-}
-
-Node.prototype.getPrevious = function() {
-	return this.previous;
-}
-
 var LinkedList = function () {
 	if (!(this instanceof arguments.callee)) {
 		throw new Error("Constructor called as a function");
 	}
-	
-	this.head = null;
-	this.tail = null;
-}
 
-LinkedList.prototype.add = function(value)
-{
-	var node = new Node(value);
-	
-	if (this.head == null) { 
-		this.head = node; 
-	}
-	
-	if (this.tail != null) { 
-		this.tail.setNext(node);
-		node.setPrevious(this.tail);
-	}
-	
-	this.tail = node;
-}
+	var Node = function (v) {
+		if (!(this instanceof arguments.callee)) {
+			throw new Error("Constructor called as a function");
+		}
 
-LinkedList.prototype.insertAfter = function(findValue, value)
-{
-	var newNode = new Node(value);
-	var node = new Node(findValue);
-	
-	for(var currentNode = this.head; currentNode; currentNode = currentNode.getNext())
-	{
-		if(currentNode.getValue() === node.getValue())
-		{			
-			if(this.tail != null && this.tail === currentNode)
-			{
-				this.tail.setNext(newNode);
-				newNode.setPrevious(this.tail);
-				this.tail = newNode;
+		var _value = v,
+		_next = null,
+		_previous = null;
+
+		return {
+			setValue: function(v) {
+				_value = v;
+			},
+			getValue: function() {
+				return _value;
+			},
+			getNext: function() {
+				return _next;
+			},
+			setNext: function(n) {
+				_next = n;
+			},
+			getPrevious: function() {
+				return _previous;
+			},
+			setPrevious: function(p) {
+				_previous = p;
+			},
+			asReadOnly: function() {
+				return {
+					getValue: function() {
+						return _value;
+					},
+					getNext: function() {
+						return _next != null ?
+						_next.asReadOnly() :
+						null;
+					},
+					getPrevious: function() {
+						return _previous != null ?
+						_previous.asReadOnly() :
+						null;
+					},
+				}
 			}
-			else
-			{
-				var nextNode = currentNode.getNext();
-				nextNode.setPrevious(newNode)				
-				
-				currentNode.setNext(newNode);
-				
-				newNode.setNext(nextNode);
-				newNode.setPrevious(currentNode);
+		}
+	}
+	var _size = 0,
+	_head = null,
+	_tail = null;
+
+	return {
+		getSize: function() {
+			return _size;
+		},
+		getHead: function() {
+			return _head != null ?
+			_head.asReadOnly() :
+			null;
+		},
+		getTail: function() {
+			return _tail != null ?
+			_tail.asReadOnly() :
+			null;
+		},
+		add: function(value) {
+			var node = new Node(value);
+
+			if (_head == null) {
+				_head = node;
 			}
-			
-			break;
+
+			if (_tail != null) {
+				_tail.setNext(node);
+				node.setPrevious(_tail);
+			}
+
+			_tail = node;
+			_size = _size + 1;
+		},
+		insertAfter: function(findValue, value) {
+			var newNode = new Node(value);
+			var node = new Node(findValue);
+
+			for(var currentNode = _head; currentNode; currentNode = currentNode.getNext()) {
+				if(currentNode.getValue() === node.getValue()) {
+					if(_tail != null && _tail === currentNode) {
+						_tail.setNext(newNode);
+						newNode.setPrevious(_tail);
+						_tail = newNode;
+						_size = _size + 1;
+					} else {
+						var nextNode = currentNode.getNext();
+						nextNode.setPrevious(newNode)
+
+						currentNode.setNext(newNode);
+
+						newNode.setNext(nextNode);
+						newNode.setPrevious(currentNode);
+						_size = _size + 1;
+					}
+
+					break;
+				}
+			}
+		},
+		insertBefore: function(findValue, value) {
+			var newNode = new Node(value);
+			var node = new Node(findValue);
+
+			for(var currentNode = _head; currentNode; currentNode = currentNode.getNext()) {
+				if(currentNode.getValue() === node.getValue()) {
+					if(_head != null && _head === currentNode) {
+						_head.setPrevious(newNode);
+						newNode.setNext(_head);
+						_head = newNode;
+						_size = _size + 1;
+					} else {
+						var previousNode = currentNode.getPrevious();
+						previousNode.setNext(newNode)
+
+						currentNode.setPrevious(newNode);
+
+						newNode.setNext(currentNode);
+						newNode.setPrevious(previousNode);
+						_size = _size + 1;
+					}
+
+					break;
+				}
+			}
+		},
+		find: function(value) {
+			for(var currentNode = _head; currentNode; currentNode = currentNode.getNext()) {
+				if(currentNode.getValue() === value) {
+					return currentNode.asReadOnly();
+				}
+			}
+		},
+		remove: function(findValue) {
+			var node = new Node(findValue);
+
+			for(var currentNode = _head; currentNode; currentNode = currentNode.getNext()) {
+				if(currentNode.getValue() === node.getValue()) {
+					if(_head != null && _head === currentNode && _tail != null && _tail === currentNode) {
+						currentNode.setPrevious(null);
+						currentNode.setNext(null);
+						_head = null;
+						_tail = null;
+						_size = _size - 1;
+					} else if(_head != null && _head === currentNode) {
+						var nextNode = currentNode.getNext();
+						nextNode.setPrevious(null);
+						_head = nextNode;
+						_size = _size - 1;
+					} else if(_tail != null && _tail === currentNode) {
+						var previousNode = currentNode.getPrevious();
+						previousNode.setNext(null);
+						_tail = previousNode;
+						_size = _size - 1;
+					} else {
+						var nextNode = currentNode.getNext();
+						var previousNode = currentNode.getPrevious();
+
+						nextNode.setPrevious(previousNode);
+						previousNode.setNext(nextNode);
+						_size = _size - 1;
+					}
+
+					break;
+				}
+			}
+		},
+		replace: function(findValue, value) {
+			var node = new Node(findValue);
+
+			for(var n = _head; n; n = n.getNext()) {
+				if(n.getValue() === node.getValue()) {
+					n.setValue(value);
+					break;
+				}
+			}
 		}
 	}
 }
-
-LinkedList.prototype.insertBefore = function(findValue, value)
-{
-	var newNode = new Node(value);
-	var node = new Node(findValue);
-	
-	for(var currentNode = this.head; currentNode; currentNode = currentNode.getNext())
-	{
-		if(currentNode.getValue() === node.getValue())
-		{			
-			if(this.head != null && this.head === currentNode)
-			{
-				this.head.setPrevious(newNode);
-				newNode.setNext(this.head);
-				this.head = newNode;
-			}
-			else
-			{
-				var previousNode = currentNode.getPrevious();
-				previousNode.setNext(newNode)				
-				
-				currentNode.setPrevious(newNode);
-				
-				newNode.setNext(currentNode);
-				newNode.setPrevious(previousNode);
-			}
-			
-			break;
-		}
-	}
-}
-
-LinkedList.prototype.remove = function(findValue)
-{
-	var node = new Node(findValue);
-	
-	for(var currentNode = this.head; currentNode; currentNode = currentNode.getNext())
-	{
-		if(currentNode.getValue() === node.getValue())
-		{			
-			if(this.head != null && this.head === currentNode && this.tail != null && this.tail === currentNode) {
-				currentNode.setPrevious(null);
-				currentNode.setNext(null);
-				this.head = null;
-				this.tail = null
-			}
-			else if(this.head != null && this.head === currentNode) {
-				var nextNode = currentNode.getNext();
-				nextNode.setPrevious(null);
-				this.head = nextNode;
-			}
-			else if(this.tail != null && this.tail === currentNode) {
-				var previousNode = currentNode.getPrevious();
-				previousNode.setNext(null);
-				this.tail = previousNode;
-			}
-			else {
-				var nextNode = currentNode.getNext();
-				var previousNode = currentNode.getPrevious();
-				
-				nextNode.setPrevious(previousNode);
-				previousNode.setNext(nextNode);
-			}
-			
-			break;
-		}
-	}
-}
-
-LinkedList.prototype.replace = function(findValue, value)
-{	
-	var node = new Node(findValue);
-	
-	for(var n = ll.head; n; n = n.getNext())
-	{
-		if(n.getValue() === node.getValue())
-		{
-			n.setValue(value);			
-			break;
-		}
-	}
-}
-
-// var ll = new LinkedList();
-// ll.add(1);
-// ll.add(2);
-// ll.add(3);
-// ll.add(4);
-// ll.add(5);
-
-// for(var n = ll.head; n; n = n.getNext())
-	// console.log('Value: '+ n.getValue() + ' | Previous: ' + (n.getPrevious() == null ? '' : n.getPrevious().getValue()));
-
-// // ll.remove(3);	
-// // ll.insertBefore(3, 2.5);
-// // ll.insertAfter(3, 4);
-// // ll.replace(3, 4);
-
-// for(var n = ll.head; n; n = n.getNext())
-	// console.log('Value: '+ n.getValue() + ' | Previous: ' + (n.getPrevious() == null ? '' : n.getPrevious().getValue()));
---
+++ tests.js
@@ -0,0 +1,253 @@
+$(document).ready( function () {
+	test('new linked lists will have zero nodes', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		equals(0, ll.getSize());
+	});
+	test('linked list with one node will have size 1', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+		equals(1, ll.getSize());
+	});
+	test('linked list with one node, getHead() will recent last return value', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+		equals('Value1', ll.getHead().getValue());
+	});
+	test('linked list with one node, getTail() will return last added value', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+		equals('Value1', ll.getTail().getValue());
+	});
+	test('linked list with 2 nodes size equals 2', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+		ll.add('Value2');
+
+		equals(2, ll.getSize());
+	});
+	test('linked list with 2 nodes, getHead() will return first value added', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+		ll.add('Value2');
+
+		equals('Value1', ll.getHead().getValue());
+	});
+	test('linked list with 2 nodes, getTail() is the last value added', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+		ll.add('Value2');
+
+		equals('Value2', ll.getTail().getValue());
+	});
+	test('linked list with two nodes, getHead() will point to tail', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+		ll.add('Value2');
+
+		equals(ll.getHead().getNext().getValue(), ll.getTail().getValue());
+	});
+	test('linked list with 3 sequence values is consistent', function () {
+		expect(3);
+
+		var ll = new LinkedList();
+		ll.add(1);
+		ll.add(2);
+		ll.add(3);
+
+		equals(1, ll.getHead().getValue());
+		equals(2, ll.getHead().getNext().getValue());
+		equals(3, ll.getHead().getNext().getNext().getValue());
+	});
+	test('it is possible to add a value after another value', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+		ll.insertAfter('Value1', 'Value2');
+
+		equals('Value2', ll.getHead().getNext().getValue());
+	});
+	test('when adding a value after another value length should be updated', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+		ll.insertAfter('Value1', 'Value2');
+
+		equals(2, ll.getSize());
+	});
+	test('it is possible to add a value before another value', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+		ll.insertBefore('Value1', 'Value2');
+
+		equals('Value2', ll.getHead().getValue());
+	});
+	test('when adding a value before another value length should be updated', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+		ll.insertBefore('Value1', 'Value2');
+
+		equals(2, ll.getSize());
+	});
+	test('it is possible to find a node by value', function () {
+		expect(1);
+
+		var node = null,
+		ll = new LinkedList();
+
+		ll.add('Value1');
+		ll.add('Value2');
+		ll.add('Value3');
+
+		node = ll.find('Value3');
+
+		equals('Value3', node.getValue());
+	});
+	test('it is possible to iterate over a linked list', function () {
+		expect(3);
+
+		var iteration = 0,
+		ll = new LinkedList();
+
+		ll.add('Value1');
+		ll.add('Value2');
+		ll.add('Value3');
+
+		for (var n = ll.getHead(); n; n = n.getNext()) {
+			if (iteration == 0)
+				equals('Value1', n.getValue());
+			if (iteration == 1)
+				equals('Value2', n.getValue());
+			if (iteration == 2)
+				equals('Value3', n.getValue());
+			iteration++;
+		}
+	});
+	test('it is possible to replace a node by value', function () {
+		expect(2);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+
+		ll.replace('Value1', 'Value2');	
+
+		equals(null, ll.find('Value1'));
+		notEqual(null, ll.find('Value2'));
+	});
+	test('it is possible to remove a node by value', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+		ll.add('Value2');
+		ll.add('Value3');
+
+		ll.remove('Value1');	
+
+		equals(null, ll.find('Value1'));
+	});
+	test('if all nodes of are removed, length should be 0', function () {
+		expect(1);
+
+		var ll = new LinkedList();
+		ll.add('Value1');
+		ll.add('Value2');
+		ll.add('Value3');
+
+		ll.remove('Value1');
+		ll.remove('Value2');
+		ll.remove('Value3');
+
+		equals(0, ll.getSize());
+	});
+	test('node returned by find() is read-only', function () {
+		expect(5);
+
+		var node = null,
+		ll = new LinkedList();
+		ll.add('Value1');
+		ll.add('Value2');
+		ll.add('Value3');
+
+		node = ll.find('Value3');
+
+		checkReadOnly(node);
+	});
+	test('Node.getNext() should return a read-only node', function () {
+		expect(5);
+
+		var ll = new LinkedList(),
+		node = null,
+		nextNode = null;
+
+		ll.add('Value1');
+		ll.add('Value2');
+
+		node = ll.find('Value1');
+		nextNode = node.getNext();
+
+		checkReadOnly(nextNode);
+	});
+	test('Node.getPrevious() should return a read-only node', function () {
+		expect(5);
+
+		var ll = new LinkedList(),
+		node = null,
+		previousNode = null;
+
+		ll.add('Value1');
+		ll.add('Value2');
+
+		node = ll.find('Value2');
+		previousNode = node.getPrevious();
+
+		checkReadOnly(previousNode);
+	});
+	test('LinkedList.getHead() should return a read-only node', function () {
+		expect(5);
+
+		var ll = new LinkedList();
+
+		ll.add('Value1');
+
+		checkReadOnly(ll.getHead());
+	});
+	test('LinkedList.getTail() should return a read-only node', function () {
+		expect(5);
+
+		var ll = new LinkedList();
+
+		ll.add('Value1');
+
+		checkReadOnly(ll.getTail());
+	});
+	function checkReadOnly(node) {
+		ok(typeof (node.setNext) == 'undefined');
+		ok(typeof (node.setPrevious) == 'undefined');
+		ok(typeof (node.getNext) == 'function');
+		ok(typeof (node.getPrevious) == 'function');
+		ok(typeof (node.getValue) == 'function');
+	}
+
+});
